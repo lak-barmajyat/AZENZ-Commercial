6e3795acbbf751f0af197421dc14a3c4b24c6737
Abdel Fattah Laktaoui <abdalfattahlaktaoui98@gmail.com>
2026-06-01 21:47:13 +0100

    add achats window

--- modules/table_de_bord/table_de_bord_view.py
+++ modules/table_de_bord/table_de_bord_view.py
@@ -120,7 +120,7 @@ class DashboardMenuView(QMainWindow):
 
         add_page(self.DashboardButton, dashboard_widget)
         add_page(self.VentesButton, ventes_widget)
-        add_page(self.ListeVentesButton, ventes_widget)
+        add_page(dashboard_widget.ListeVentesButton, ventes_widget)
         add_page(self.ClientsButton)
         add_page(self.FournisseursButton)
         add_page(self.ArticlesButton)
